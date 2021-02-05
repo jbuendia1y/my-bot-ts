70e39199d300ea91d0de389510347e206a9dde36
jbuendia1y <jgamer669@gmail.com>
2021-02-05 15:11:56 -0500

    events of player

--- src/events.ts
+++ src/events.ts
@@ -1,33 +1,42 @@
-import { Client, Message } from 'discord.js'
 import { prefix } from './config.json'
-import { msgEmbed } from './recyclable/msgembed'
+import { Client, Message } from 'discord.js'
 import { Player } from 'discord-player'
+
+import { msgEmbed } from './recyclable/msgembed'
 
 const client: Client = new Client()
 const player = new Player(client)
 
-player.on('trackStart', (message, track) => {
-    const title = `Now Playing ${track.title}`
-    const embed = msgEmbed(track.url, title, track.description)
+player
+    .on('trackStart', (message, track) => {
+        const title = `Now Playing ${track.title}`
+        const embed = msgEmbed(track.url, title, track.description)
 
-    message.channel.send(embed)
-})
-
-player.on('trackAdd', (message, track) => {
-    const song = track.tracks[track.tracks.length - 1]
-    const title = `Add to Queue ${song.title}`
-    const embed = msgEmbed(song.url, title, song.description)
-    message.channel.send(embed)
-})
+        message.channel.send(embed)
+    })
+    .on('trackAdd', (message, track) => {
+        const song = track.tracks[track.tracks.length - 1]
+        const title = `Add to Queue ${song.title}`
+        const embed = msgEmbed(song.url, title, song.description)
+        message.channel.send(embed)
+    })
     .on('queueEnd', (message, track) => {
         message.channel.send(`Queue END - ${track.tracks.length - 1} songs`)
     })
-    .on('botDisconnect',(message)=>{
+    .on('botDisconnect', (message) => {
         message.channel.send('aaaaadios')
+    })
+    .on('error', (err, message) => {
+        message.channel.send(err)
+    })
+    .on('searchInvalidResponse', (message, query, tracks, err, collector) => {
+        console.log(err)
     })
 
 client.on('message', async (message: Message) => {
-    if (message.content.startsWith(`${prefix}play`)) {
+    const comand = message.content.toLocaleLowerCase()
+
+    if (comand.startsWith(`${prefix}play`)) {
         const msg = message.content
         if (message.member?.voice.channel) {
 
@@ -43,18 +52,29 @@ client.on('message', async (message: Message) => {
         return message.channel.send('GO TO THE CHANNEL VOICE')
     }
 
-    if (message.content.startsWith(`${prefix}queue`) || message.content.startsWith(`${prefix}q`)) {
-        if (player.getQueue(message).tracks) {
+    if (comand.startsWith(`${prefix}queue`) || comand.startsWith(`${prefix}q`)) {
+        if (player.shuffle(message).tracks) {
             for (const track of player.getQueue(message).tracks) {
-                message.channel.send(`${track.durationMS} - ${track.duration}`)
-                return message.channel.send(msgEmbed(track.url, track.title, track.description))
+                const embed = msgEmbed(track.url, track.title, track.description)
+                message.channel.send(embed)
             }
         }
-        return message.channel.send('0 Songs')
     }
 
-    if (message.content.startsWith(`${prefix}leave`)) {
-        message.member?.voice.channel?.leave()
+    if (comand.startsWith(`${prefix}skip`)) {
+        player.skip(message)
+    }
+
+    if (comand.startsWith(`${prefix}leave`)) {
+        player.stop(message)
+    }
+
+    if (comand.startsWith(`${prefix}pause`)) {
+        player.pause(message)
+    }
+
+    if (comand.startsWith(`${prefix}resume`)) {
+        player.resume(message)
     }
 })
 
